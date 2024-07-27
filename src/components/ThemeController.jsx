9c51a7887d7750cf264adcc8b33b93eb92cd1916
Muhammad Tahir <mt@technoholicas.com>
2024-07-27 14:08:42 +0500

    Add theme controller

--- src/components/ThemeController.jsx
+++ src/components/ThemeController.jsx
@@ -4,7 +4,7 @@ const ThemeController = () => {
   return (
     <label className="swap swap-rotate">
       {/* this hidden checkbox controls the state */}
-      <input type="checkbox" className="theme-controller" value="synthwave" />
+      <input type="checkbox" className="theme-controller" value="light" />
 
       {/* sun icon */}
       <svg
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -3,14 +3,14 @@ import * as Yup from "yup";
 const ValidateRegistrationForm = Yup.object({
   firstName: Yup.string()
     .max(15, "Must be 15 characters or less")
-    .required("Required"),
+    .required("First Name is required"),
   lastName: Yup.string()
     .max(20, "Must be 20 characters or less")
-    .required("Required"),
-  email: Yup.string().email("Invalid email address").required("Required"),
+    .required("Last Name is required"),
+  email: Yup.string().email("Invalid email address").required("Email is Required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
-    .required("Required"),
+    .required("Password is required"),
 });
 
 const ValidateLoginForm = Yup.object({
