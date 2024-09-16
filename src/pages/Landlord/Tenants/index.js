3adbbd6578aa493c50b57aeb04ec4914fd1a1a6e
Muhammad Tahir <mt@technoholicas.com>
2024-09-16 17:02:37 -0500

    Add smooth grid drag and drop

--- src/pages/Landlord/Tenants/index.js
+++ src/pages/Landlord/Tenants/index.js
@@ -4,41 +4,20 @@ import {
   GridDropZone,
   GridItem,
   swap,
-  move,
 } from "react-grid-dnd";
 
 const Tenants = () => {
-  const [items, setItems] = React.useState({
-    left: [
-      { position: 0, name: "ben" },
-      { position: 1, name: "joe" },
-      { position: 2, name: "jason", size: 2 },
-      { position: 3, name: "chris" },
-      { position: 4, name: "heather" },
-    ],
-  });
+  const [items, setItems] = React.useState([
+    { position: 0, name: "ben" },
+    { position: 1, name: "joe" },
+    { position: 2, name: "jason", size: 2 },
+    { position: 3, name: "chris" },
+    { position: 4, name: "heather" },
+  ]);
 
   function onChange(sourceId, sourceIndex, targetIndex, targetId) {
-    console.log(targetId, sourceId);
-    if (targetId) {
-      const result = move(
-        items[sourceId],
-        items[targetId],
-        sourceIndex,
-        targetIndex
-      );
-      return setItems({
-        ...items,
-        [sourceId]: result[0],
-        [targetId]: result[1],
-      });
-    }
-
-    const result = swap(items[sourceId], sourceIndex, targetIndex);
-    return setItems({
-      ...items,
-      [sourceId]: result,
-    });
+    const nextState = swap(items, sourceIndex, targetIndex);
+    setItems(nextState);
   }
 
   return (
@@ -50,7 +29,7 @@ const Tenants = () => {
           boxesPerRow={2}
           rowHeight={300}
         >
-          {items.left.map((item) => (
+          {items.map((item) => (
             <GridItem key={item.position}>
               <div className="grid-item w-full h-full p-2 box-border">
                 <div className="grid-item-content w-full h-full bg-blue-500 flex items-center justify-center text-white font-sans rounded-lg">
