5cfc397418389208acaa1f6138ed631426b561ae
Muhammad Tahir <mt@technoholicas.com>
2024-09-16 16:39:50 -0500

    Add smooth grid drag and drop

--- src/pages/Landlord/Prospects/index.js
+++ src/pages/Landlord/Prospects/index.js
@@ -1,32 +1,75 @@
-import React, { useState } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import Widget from '../../../components/Widget';
+import { useState } from "react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { motion } from 'framer-motion';
 
-const Prospect = () => {
-  const initialWidgets = ['1', '2', '3', '4', '5', '6', '7', '8'];
-  const [widgets, setWidgets] = useState(initialWidgets);
+const ItemType = "WIDGET";
 
-  const moveWidget = (fromId, toId) => {
-    const fromIndex = widgets.indexOf(fromId);
-    const toIndex = widgets.indexOf(toId);
+const Widget = ({ id, text, index, moveWidget }) => {
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemType,
+    item: { index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
 
+  const [, drop] = useDrop({
+    accept: ItemType,
+    hover: (item) => {
+      if (item.index !== index) {
+        moveWidget(item.index, index);
+        item.index = index;
+      }
+    },
+  });
+
+  return (
+    <motion.div
+      ref={(node) => drag(drop(node))}
+      className={`widget bg-gray-200  p-4 transition-all text-center ${
+        isDragging ? "opacity-50" : "opacity-100"
+      }`}
+    >
+      {text}
+      </motion.div>
+  );
+};
+
+const Dashboard = () => {
+  const [widgets, setWidgets] = useState([
+    { id: 1, text: "Widget 1" },
+    { id: 2, text: "Widget 2" },
+    { id: 3, text: "Widget 3" },
+    { id: 4, text: "Widget 4" },
+    { id: 5, text: "Widget 5" },
+    { id: 6, text: "Widget 6" },
+    { id: 7, text: "Widget 7" },
+    { id: 8, text: "Widget 8" },
+  ]);
+
+  const moveWidget = (fromIndex, toIndex) => {
     const updatedWidgets = [...widgets];
-    updatedWidgets[fromIndex] = toId;
-    updatedWidgets[toIndex] = fromId;
-
+    const [removed] = updatedWidgets.splice(fromIndex, 1);
+    updatedWidgets.splice(toIndex, 0, removed);
     setWidgets(updatedWidgets);
   };
 
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="grid grid-cols-2 grid-rows-4 gap-4 p-4">
-        {widgets.map((id) => (
-          <Widget key={id} id={id} moveWidget={moveWidget} />
+        {widgets.map((widget, index) => (
+          <Widget
+            key={widget.id}
+            id={widget.id}
+            text={widget.text}
+            index={index}
+            moveWidget={moveWidget}
+          />
         ))}
       </div>
     </DndProvider>
-  )
-}
+  );
+};
 
-export default Prospect;
+export default Dashboard;
--- src/pages/Landlord/Tenants/index.js
+++ src/pages/Landlord/Tenants/index.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import {
+  GridContextProvider,
+  GridDropZone,
+  GridItem,
+  swap,
+  move,
+} from "react-grid-dnd";
 
 const Tenants = () => {
+  const [items, setItems] = React.useState({
+    left: [
+      { position: 0, name: "ben" },
+      { position: 1, name: "joe" },
+      { position: 2, name: "jason", size: 2 },
+      { position: 3, name: "chris" },
+      { position: 4, name: "heather" },
+    ],
+  });
+
+  function onChange(sourceId, sourceIndex, targetIndex, targetId) {
+    console.log(targetId, sourceId);
+    if (targetId) {
+      const result = move(
+        items[sourceId],
+        items[targetId],
+        sourceIndex,
+        targetIndex
+      );
+      return setItems({
+        ...items,
+        [sourceId]: result[0],
+        [targetId]: result[1],
+      });
+    }
+
+    const result = swap(items[sourceId], sourceIndex, targetIndex);
+    return setItems({
+      ...items,
+      [sourceId]: result,
+    });
+  }
+
   return (
-    <div>Landlord Tenants</div>
-  )
-}
+    <GridContextProvider onChange={onChange}>
+      <div className="flex justify-center mx-auto p-3 h-full">
+        <GridDropZone
+          className="dropzone left flex-1 h-full border border-dashed border-gray-300 rounded-lg bg-green-500"
+          id="left"
+          boxesPerRow={2}
+          rowHeight={300}
+        >
+          {items.left.map((item) => (
+            <GridItem key={item.position}>
+              <div className="grid-item w-full h-full p-2 box-border">
+                <div className="grid-item-content w-full h-full bg-blue-500 flex items-center justify-center text-white font-sans rounded-lg">
+                  {item.name.toUpperCase()}
+                </div>
+              </div>
+            </GridItem>
+          ))}
+        </GridDropZone>
+      </div>
+    </GridContextProvider>
+  );
+};
 
 export default Tenants;
